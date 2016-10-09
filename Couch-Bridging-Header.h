65910eda5cb57a7a029473bfaa76caf9cedaba60
Abhi Khanal <abhi@imoonlight.io>
2016-10-09 12:58:34 -0400

    Initialize commit

---
+++ Couch-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Couch-Bridging-Header.h
+//  Couch
+//
+//  Created by Abhinash Khanal on 10/9/16.
+//  Copyright © 2016 Moonlighting. All rights reserved.
+//
+
+#ifndef Couch_Bridging_Header_h
+#define Couch_Bridging_Header_h
+
+#import <CouchbaseLite/CouchbaseLite.h>
+
+
+#endif /* Couch_Bridging_Header_h */
--- Couch/ViewController.swift
+++ Couch/ViewController.swift
@@ -13,11 +13,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.crudtest()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func crudtest() {
+        let manager: CBLManager = CBLManager()
+        do {
+            let database = try manager.databaseNamed("stuff")
+            let document = database.document(withID: "1234")
+            guard let doc = document else {
+                return
+            }
+            var properties = [String: Any]()
+            properties["name"] = "Awesome!"
+            try doc.putProperties(properties)
+            
+            let newDoc = database.existingDocument(withID: "1234")
+            print(newDoc)
+            print(newDoc?.property(forKey: "name"))
+            
+            guard let newProp = newDoc?.properties else {
+                return
+            }
+            
+            properties = newProp
+            properties["name"] = "Everything is"
+            try newDoc?.putProperties(properties)
+            
+            let newDoc1 = database.existingDocument(withID: "1234")
+            print(newDoc1?.property(forKey: "name"))
+            
+            try newDoc1?.delete()
+            let newDoc2 = database.existingDocument(withID: "1234")
+            
+            print(newDoc2)
+            
+            try database.close()
+
+            
+        } catch let error as NSError {
+            print(error)
+        }
     }
 
 
